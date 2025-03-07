cd5f05ba53a50b7b28392e49cc90c046d8a27da8
Rashad Dweik <rashaddweik20@gmail.com>
2025-03-07 14:26:17 +0200

    ...

--- src/main/java/mytechshop/mytechshop/services/ProductService.java
+++ src/main/java/mytechshop/mytechshop/services/ProductService.java
@@ -1,6 +1,7 @@
 package mytechshop.mytechshop.services;
 
 import lombok.RequiredArgsConstructor;
+import mytechshop.mytechshop.models.Brand;
 import mytechshop.mytechshop.models.Category;
 import mytechshop.mytechshop.models.Product;
 import mytechshop.mytechshop.repositories.ProductRepository;
@@ -29,12 +30,47 @@ public class ProductService implements IProductService {
 
     @Override
     public List<Product> getProductsByCategory(Category category) {
-        return List.of();
+        return productRepository.findByCategory(category);
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(Brand brand) {
+        return productRepository.findByBrand(brand);
+    }
+
+    @Override
+    public List<Product> getProductsByPriceRange(Double minPrice, Double maxPrice) {
+        return productRepository.findByPriceBetween(minPrice, maxPrice);
+    }
+
+    @Override
+    public List<Product> searchProductsByName(String name) {
+        return productRepository.findByNameContainingIgnoreCase(name);
+    }
+
+    @Override
+    public List<Product> getAvailableProducts() {
+        return productRepository.findByAvailableTrue();
     }
 
     @Override
     public Product saveProduct(Product product) {
         return productRepository.save(product);
+    }
+
+    @Override
+    public Product updateProduct(Long id, Product product) {
+        return productRepository.findById(id)
+                .map(existingProduct -> {
+                    existingProduct.setName(product.getName());
+                    existingProduct.setDescription(product.getDescription());
+                    existingProduct.setPrice(product.getPrice());
+                    existingProduct.setStock(product.getStock());
+                    existingProduct.setCategory(product.getCategory());
+                    existingProduct.setBrand(product.getBrand());
+                    return productRepository.save(existingProduct);
+                })
+                .orElseThrow(() -> new RuntimeException("Product not found"));
     }
 
     @Override
