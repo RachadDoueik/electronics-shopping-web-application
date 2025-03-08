286ccebf6b39ce6382e3d53fcaffeea6a763d613
Rashad Dweik <rashaddweik20@gmail.com>
2025-03-08 13:28:15 +0200

    implemented wishlist and category services

---
+++ src/main/java/mytechshop/mytechshop/requests/UpdateCategoryRequest.java
@@ -0,0 +1,28 @@
+package mytechshop.mytechshop.requests;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UpdateCategoryRequest {
+
+    @NotBlank(message = "Category name is required")
+    private String name; // Name of the category
+
+    private String description; // Description of the category
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
